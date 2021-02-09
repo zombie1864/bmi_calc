86920119d3ce8f0ae80b6fa78f23c8f5917a9cc7
zombie1864 <zombie1864@gmail.com>
2021-02-09 18:37:52 -0500

    have functional btns that both inc currField or dec, onClick

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ interface Istate {
   name: string, 
   gender: boolean, 
   height: number, 
-  weight: number
+  weight: number,
+  currField: number
 }
 
 class App extends React.Component { 
-  state: Istate = { // global state 
-    name: 'jeff', 
+  state: Istate = { // global state, initialState
+    name: '', 
     gender: true, // using boolean for gender male: true female: false 
-    height: 5.11, 
-    weight: 202
+    height: 0, 
+    weight: 0,
+    currField: 0
   }
   render() {
-    const BMICalcProps = { name: this.state.name, gender: this.state.gender, height: this.state.height, weight: this.state.weight } // look into why this works 
+    const BMICalcProps = { name: this.state.name, gender: this.state.gender, height: this.state.height, weight: this.state.weight, currField:this.state.currField } // look into why this works 
     return (
       <div className="App">
         <h1>BMI Calculator</h1> {/* this is a comment*/}
